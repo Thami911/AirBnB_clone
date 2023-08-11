de6204284d9d9a01a9eb7754f91a4ead4a0eed3c
Thami911 <thamimncwabe@gmail.com>
2023-08-11 06:20:12 -0700

    update

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Describes the class for Amenity."""
+from models.base_models import Basemodels
+
+class Amenity(Basemodel):
+    """Presents the Amenity model.
+    Attributes:
+        name (str): The name of the amenity.
+    """
+    
+    name = ""
